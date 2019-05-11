27d48f88b71d2e2f88f670fe10ef6380130f2361
jpsietsma <jpsietsma@gmail.com>
2019-05-10 20:37:39 -0400

    Initial Commit - Added MediaClasses project, Added view models and folder structure.

---
+++ MediaClasses/ViewModels/MediaFileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MediaClasses.ViewModels
+{
+    public class MediaFileViewModel
+    {
+        [Key]
+        public int Id { get; }
+
+    }
+}
